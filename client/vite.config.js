ddeefde2b27bb5c3e8bbbc33a2d8742c40a36f49
Salman Qureshi <isalmanqureshi@gmail.com>
2025-08-20 20:38:42 +0530

    chore: Update home screenshot for improved visual representation

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 
+
 export default defineConfig(({ mode }) => {
   // Load environment variables
   const env = loadEnv(mode, '.', '');
@@ -23,4 +24,4 @@ export default defineConfig(({ mode }) => {
       __API_BASE_URL__: JSON.stringify(apiBaseUrl)
     }
   }
-})
+});
